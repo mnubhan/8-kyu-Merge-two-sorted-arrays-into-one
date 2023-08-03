071ec350a45cd2097587f439c9d96586ef865e68
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-08-03 21:14:32 +0100

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,12 @@
+function mergeArrays(arr1, arr2) {
+  return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+}
+
+function mergeArrays(a, b) {
+  return [...new Set(a.concat(b))].sort((a,b)=>a-b)
+}
+
+function mergeArrays(arr1, arr2) {
+  let arr = new Set([...arr1,...arr2].sort((a,b)=> a-b));
+  return [...arr];
+}
